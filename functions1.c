da07510e877a9f1c535d278707d44011d68e71d5
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-13 00:49:39 +0300

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -17,7 +17,7 @@ int printisha_unsigned(va_list types, char buffer[],
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 
-	num = convert_size_unsgnd(num, size);
+	num = converter_mtoto(num, size);
 
 	if (num == 0)
 		buffer[i--] = '0';
