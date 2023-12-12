77ff0c03a5c56de3eca3cb9f9af00c43bd917ef4
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-13 00:46:45 +0300

    Update main.h

--- main.h
+++ main.h
@@ -97,7 +97,7 @@ int andika_number(int is_positive, int ind, char buffer[],
 	int flags, int width, int precision, int size);
 int write_nambari(int ind, char bff[], int flags, int width, int precision,
 	int length, char padd, char extra_c);
-int write_pointer(char buffer[], int ind, int length,
+int andikisha_pointer(char buffer[], int ind, int length,
 	int width, int flags, char padd, char extra_c, int padd_start);
 
 int andika_unsgnd(int is_negative, int ind,
